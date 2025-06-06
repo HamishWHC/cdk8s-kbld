29159a17e61983f73d5ce3bccbfbbb029d12a54b
HamishWHC <20359560+HamishWHC@users.noreply.github.com>
2025-06-06 17:05:07 +1000

    v0.1.0

--- src/config.ts
+++ src/config.ts
@@ -6,78 +6,88 @@ import type { Destination, Override, SearchRule, Source } from "./types";
  * Configuration for kbld. See https://carvel.dev/kbld/docs/latest/config/
  */
 export interface KbldConfigProps {
-    /**
-     * The minimum required version of the kbld CLI.
-     *
-     * @default 0.45.0
-     */
-    minimumRequiredVersion?: string;
+	/**
+	 * The minimum required version of the kbld CLI.
+	 */
+	minimumRequiredVersion?: string;
 
-    /**
-     * A list of Search Rules that kbld follows to locate container image references within the input files.
-     *
-     * Always has the default search rule below appended.
-     *
-     * @default [{keyMatcher: {name: "image"}, updateStrategy: {entireValue: {}}}]
-     */
-    searchRules?: SearchRule[];
+	/**
+	 * A list of Search Rules that kbld follows to locate container image references within the input files.
+	 *
+	 * Always has the default search rule below appended.
+	 *
+	 * @default [{keyMatcher: {name: "image"}, updateStrategy: {entireValue: {}}}]
+	 */
+	searchRules?: SearchRule[];
 
-    /**
-     * A list of Overrides that kbld applies to found container image references before it attempts to resolve or build the actual image.
-     */
-    overrides?: Override[];
+	/**
+	 * A list of Overrides that kbld applies to found container image references before it attempts to resolve or build the actual image.
+	 */
+	overrides?: Override[];
 
-    /**
-     * A list of Sources, each of which describes where to find the source that makes up the contents of a given image and which build tool to use to construct it.
-     */
-    sources?: Source[];
+	/**
+	 * A list of Sources, each of which describes where to find the source that makes up the contents of a given image and which build tool to use to construct it.
+	 */
+	sources?: Source[];
 
-    /**
-     * A list of Destinations, each of which describes where (i.e. to which container registry) to publish a given image.
-     */
-    destinations?: Destination[];
+	/**
+	 * A list of Destinations, each of which describes where (i.e. to which container registry) to publish a given image.
+	 */
+	destinations?: Destination[];
 }
 
 export default class KbldConfig extends ApiObject {
-    /**
-     * Returns the apiVersion and kind for "io.k8s.api.core.v1.Service"
-     */
-    public static readonly GVK: GroupVersionKind = {
-        apiVersion: "kbld.k14s.io/v1alpha1",
-        kind: "Config",
-    };
+	/**
+	 * Returns the apiVersion and kind for "io.k8s.api.core.v1.Service"
+	 */
+	public static readonly GVK: GroupVersionKind = {
+		apiVersion: "kbld.k14s.io/v1alpha1",
+		kind: "Config",
+	};
 
-    public static manifest(props: KbldConfigProps = {}): any {
-        return {
-            ...KbldConfig.GVK,
-            ...toJson_KbldConfigProps(props),
-        };
-    }
+	public static manifest(props: KbldConfigProps = {}): any {
+		return {
+			...KbldConfig.GVK,
+			...props,
+		};
+	}
 
-    public constructor(scope: Construct, id: string, props: KbldConfigProps = {}) {
-        super(scope, id, {
-            ...KbldConfig.GVK,
-            ...props,
-        });
-    }
+	public constructor(
+		scope: Construct,
+		id: string,
+		props: KbldConfigProps = {},
+	) {
+		super(scope, id, {
+			...KbldConfig.GVK,
+			...omitUndefined(props),
+		});
+	}
 
-    /**
-     * Renders the object to Kubernetes JSON.
-     */
-    public override toJson(): any {
-        const resolved = super.toJson();
+	/**
+	 * Renders the object to Kubernetes JSON.
+	 */
+	public override toJson(): any {
+		const resolved = super.toJson();
 
-        return {
-            ...KbldConfig.GVK,
-            ...toJson_KbldConfigProps(resolved),
-        };
-    }
+		return {
+			...KbldConfig.GVK,
+			...omitUndefined(resolved),
+		};
+	}
 }
 
-export function toJson_KbldConfigProps(obj: KbldConfigProps | undefined): Record<string, any> | undefined {
-    if (obj === undefined) {
-        return undefined;
-    }
-
-    return Object.entries(obj).reduce((r, i) => (i[1] === undefined ? r : { ...r, [i[0]]: i[1] }), {});
-}
+const omitUndefined = (value: any): any => {
+	if (value === null || typeof value !== "object") {
+		return value;
+	} else if (Array.isArray(value)) {
+		return value.filter((v) => v !== undefined).map((el) => omitUndefined(el));
+	} else {
+		return Object.entries(value).reduce((acc, [k, v]) => {
+			if (v === undefined) {
+				return acc;
+			} else {
+				return { ...acc, [k]: omitUndefined(v) };
+			}
+		}, {});
+	}
+};
--- src/image.ts
+++
@@ -1,9 +0,0 @@
-import { Construct } from "constructs";
-
-export interface KbldImageProps {}
-
-export default class KbldImage extends Construct {
-    constructor(scope: Construct, name: string, props: KbldImageProps) {
-        super(scope, name);
-    }
-}
